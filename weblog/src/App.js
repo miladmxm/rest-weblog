ad94c84430db1fdf23d12bbd6b3e92c4d7506891
afraweb111 <afraweb111@gmail.com>
2021-10-09 15:09:54 +0330

    s new

--- weblog/src/App.js
+++ weblog/src/App.js
@@ -15,7 +15,7 @@ const App = ()=> {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts
+          Learn Reactss
         </a>
       </header>
     </div>
