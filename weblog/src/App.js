07b499b7e72163faba19a2916d37cf6fbe1fc342
miladmxm <miladmxm12@gmail.com>
2021-10-09 15:15:38 +0330

    helo

--- weblog/src/App.js
+++ weblog/src/App.js
@@ -3,22 +3,7 @@ import './App.css';
 
 const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          L
-        </a>
-      </header>
-    </div>
+   <h1>hello worls</h1>
   );
 }
 
