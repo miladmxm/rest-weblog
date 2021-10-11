fc0a071eb3c86c8fca7ea7a90155ad2ab6ce36d5
miladmxm <miladmxm12@gmail.com>
2021-10-11 12:39:43 +0330

    new dashboard

--- weblog/src/components/layouts/dashboard/common/header.jsx
+++ weblog/src/components/layouts/dashboard/common/header.jsx
@@ -2,37 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SearchForm from "./search";
 
-
 const HeaderDash = () => {
-    const [activedToggleBtn, setActivedToggleBtn] = useState(false)
-    const toggleMenu = () => {
-        setActivedToggleBtn(!activedToggleBtn)
-    }
-    return (
-        <div className="dashboard-header">
-            <div className="navbar">
-                <div className="container">
-                    <h2 className="logo"><Link className='text-initial' to="/dashboard">میلاد حسیبی</Link></h2>
-                    <button className={activedToggleBtn ? "toggle toggler" : "toggle"} onClick={toggleMenu}>
-                        <span></span>
-                    </button>
-
-                    <nav id="navMenu" className={activedToggleBtn ? "showNav" : ""}>
-                        <ul>
-                            <li><Link className="active" to="/dashboard">خانه</Link></li>
-                            <li><Link to="/add-post">افزودن پست</Link></li>
-
-                            <li><Link to="/">مشاهده وبلاگ</Link></li>
-                            <li><Link to="/login">خروج</Link></li>
-                            <li>
-                                <SearchForm />
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-
-    )
-}
-export default HeaderDash
+  const [activedToggleBtn, setActivedToggleBtn] = useState(false);
+  const toggleMenu = () => {
+    setActivedToggleBtn(!activedToggleBtn);
+  };
+  return (
+    <header className="fixed-top">
+    <div className="dashboard-header">
+      <div>
+        <h4 className="tooltip" data-tooltip="مشاهده وبلاگ"><Link target="_blank" to="/">میلاد حسیبی</Link></h4>
+      </div>
+      <SearchForm />
+      <button type="button" className="btn">
+        خروج
+      </button>
+    </div>
+    </header>
+  );
+};
+export default HeaderDash;
--- weblog/src/components/layouts/dashboard/dashlayout.jsx
+++ weblog/src/components/layouts/dashboard/dashlayout.jsx
@@ -1,58 +1,125 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const dashboard = () => {
-    return (
-        <>
-            <div id="dashboard">
-                <section className="content">
-                    <div className="table">
-                        <table className="tableShowItem">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>عنوان پست</th>
-                                    <th>تاریخ ثبت پست</th>
-                                    <th>وضعیت</th>
-                                    <th className="textCenter">ویرایش | حذف</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                <tr>
-                                    <td>1</td>
-                                    <td><Link to="#">لورم ایپسوم</Link></td>
-                                    <td>25 بهمن 1400</td>
-                                    <td><span className="badge badge-denger">خصوصی</span></td>
-                                    <td className="textCenter editDelete"><Link to="#"><i className="fa fa-edit"></i></Link> | <Link to=""><i className="fa fa-trash"></i></Link></td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td><Link to="#">لورم ایپسوم</Link></td>
-                                    <td>25 بهمن 1400</td>
-                                    <td><span className="badge">خصوصی</span></td>
-                                    <td className="textCenter editDelete"><Link to="#"><i className="fa fa-edit"></i></Link> | <Link to=""><i className="fa fa-trash"></i></Link></td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td><Link to="#">لورم ایپسوم</Link></td>
-                                    <td>25 بهمن 1400</td>
-                                    <td><span className="badge">خصوصی</span></td>
-                                    <td className="textCenter editDelete"><Link to="#"><i className="fa fa-edit"></i></Link> | <Link to=""><i className="fa fa-trash"></i></Link></td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td><Link to="#">لورم ایپسوم</Link></td>
-                                    <td>25 بهمن 1400</td>
-                                    <td><span className="badge badge-denger">عمومی</span></td>
-                                    <td className="textCenter editDelete"><Link to="#"><i className="fa fa-edit"></i></Link> | <Link to=""><i className="fa fa-trash"></i></Link></td>
-                                </tr>
-
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
-            </div>
-        </>
-    )
-}
-export default dashboard
+  return (
+    <>
+      <div id="dashboard">
+        <aside className="side-bar">
+        <header>داشبورد وبلاگ <img stc="./img/logo" /></header>
+          <nav>
+            <ul>
+              <li>
+                <Link className="active" href="">
+                <i className="fa fa-book"></i>
+                  پست ها
+                </Link>
+              </li>
+              <li>
+              <i className="fa fa-plus"></i>
+                <Link href="">افزودن پست</Link>
+              </li>
+              <li>
+              <i className="fa fa-cog"></i>
+                <Link href="">تنظیمات حساب</Link>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <section className="content">
+          <div className="table">
+            <table className="tableShowItem">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>عنوان پست</th>
+                  <th>تاریخ ثبت پست</th>
+                  <th>وضعیت</th>
+                  <th className="textCenter">ویرایش | حذف</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>
+                    <Link to="#">لورم ایپسوم</Link>
+                  </td>
+                  <td>25 بهمن 1400</td>
+                  <td>
+                    <span className="badge badge-denger">خصوصی</span>
+                  </td>
+                  <td className="textCenter editDelete">
+                    <Link to="#">
+                      <i className="fa fa-edit"></i>
+                    </Link>{" "}
+                    |{" "}
+                    <Link to="">
+                      <i className="fa fa-trash"></i>
+                    </Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>
+                    <Link to="#">لورم ایپسوم</Link>
+                  </td>
+                  <td>25 بهمن 1400</td>
+                  <td>
+                    <span className="badge">خصوصی</span>
+                  </td>
+                  <td className="textCenter editDelete">
+                    <Link to="#">
+                      <i className="fa fa-edit"></i>
+                    </Link>{" "}
+                    |{" "}
+                    <Link to="">
+                      <i className="fa fa-trash"></i>
+                    </Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>
+                    <Link to="#">لورم ایپسوم</Link>
+                  </td>
+                  <td>25 بهمن 1400</td>
+                  <td>
+                    <span className="badge">خصوصی</span>
+                  </td>
+                  <td className="textCenter editDelete">
+                    <Link to="#">
+                      <i className="fa fa-edit"></i>
+                    </Link>{" "}
+                    |{" "}
+                    <Link to="">
+                      <i className="fa fa-trash"></i>
+                    </Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>
+                    <Link to="#">لورم ایپسوم</Link>
+                  </td>
+                  <td>25 بهمن 1400</td>
+                  <td>
+                    <span className="badge badge-denger">عمومی</span>
+                  </td>
+                  <td className="textCenter editDelete">
+                    <Link to="#">
+                      <i className="fa fa-edit"></i>
+                    </Link>{" "}
+                    |{" "}
+                    <Link to="">
+                      <i className="fa fa-trash"></i>
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+export default dashboard;
