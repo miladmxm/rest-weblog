b2464f487ea0f6ebf8780dd533245c33c96844b6
miladmxm <miladmxm12@gmail.com>
2021-10-18 10:04:34 +0330

    update truncate

--- weblog/src/components/utils/truncate.js
+++ weblog/src/components/utils/truncate.js
@@ -1,6 +1,7 @@
 export const truncate = (str, len) => {
-    let newTxt = str.replace(/<[a-zA-Z & 0-9 & ; & / & ' & " & \ & = & ( & ) & ! & * & %]*?>/g, ' ')
-    
-        return newTxt.length > len-10 ? newTxt.substring(0, len) + "..." : newTxt;
-    
-}
+  
+  let newTxt = str.replace(/<.*?>/g, " ");
+  newTxt = newTxt.replace(/&.*?;/g, " ");
+
+  return newTxt.length > len - 10 ? newTxt.substring(0, len) + "..." : newTxt;
+};
